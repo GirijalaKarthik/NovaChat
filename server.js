f52bedd6665db919f0aaf4b4aa839b15985fe81a
Girijala Karthik <karthikgirijala96@gmail.com>
2026-02-15 01:34:53 +0530

    y

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static("Public"));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "Public", "Index.html"));
-});
+app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "Public", "Index.html")); });
 
 let onlineUsers = {}; 
+let protectedGroups = {}; 
 
 io.on("connection", (socket) => {
     
@@ -25,39 +23,80 @@ io.on("connection", (socket) => {
             if (!err && results.length > 0) {
                 const user = results[0];
                 socket.username = user.username;
+                socket.role = user.role;
                 onlineUsers[user.username] = socket.id;
                 
-                socket.emit("login_response", { success: true, username: user.username });
-                io.emit("update_user_list", Object.keys(onlineUsers));
-            } else {
-                socket.emit("login_response", { success: false, msg: "Invalid Credentials" });
-            }
+                socket.emit("login_response", { success: true, role: user.role, username: user.username });
+                io.emit("update_lists", { users: Object.keys(onlineUsers), groups: Object.keys(protectedGroups) });
+            } else { socket.emit("login_response", { success: false, msg: "Invalid Credentials" }); }
         });
+    });
+
+    socket.on("register_request", (data) => {
+        const sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
+        db.query(sql, [data.username, data.password, data.role], (err) => {
+            if (err) socket.emit("register_response", { success: false, msg: "Username Taken" });
+            else socket.emit("register_response", { success: true, msg: "Success! Launch now." });
+        });
+    });
+
+    socket.on("create_group", (data) => {
+        if (socket.role === 'admin') {
+            protectedGroups[data.name] = { 
+                password: data.password, 
+                limit: data.limit, 
+                members: [socket.username], 
+                admin: socket.username 
+            };
+            io.emit("update_lists", { users: Object.keys(onlineUsers), groups: Object.keys(protectedGroups) });
+        }
+    });
+
+    socket.on("join_group", (data) => {
+        const group = protectedGroups[data.name];
+        if (group.password === data.password) {
+            if (group.members.length < group.limit) {
+                if (!group.members.includes(socket.username)) group.members.push(socket.username);
+                socket.emit("group_join_success", { name: data.name });
+            } else { socket.emit("group_response", { success: false, msg: "Group Full!" }); }
+        } else { socket.emit("group_response", { success: false, msg: "Wrong Password!" }); }
     });
 
     socket.on("send_message", (data) => {
         const { sender, msg, toUser, type } = data;
-        const sql = "INSERT INTO chats (sender_name, message, to_user, type) VALUES (?, ?, ?, ?)";
-        db.query(sql, [sender, msg, toUser, type], (err, result) => {
-            if (!err) {
-                const messageData = { id: result.insertId, sender, msg, toUser, type };
-                if (type === 'private') {
-                    if (onlineUsers[toUser]) io.to(onlineUsers[toUser]).emit("receive_message", messageData);
-                    socket.emit("receive_message", messageData);
-                } else {
-                    io.emit("receive_message", messageData);
-                }
+        const messageData = { sender, msg, toUser, type, role: socket.role, id: Date.now() };
+
+        if (type === 'private') {
+            if (onlineUsers[toUser]) io.to(onlineUsers[toUser]).emit("receive_message", messageData);
+            socket.emit("receive_message", messageData);
+        } else if (type === 'group') {
+            if (protectedGroups[toUser] && protectedGroups[toUser].admin === sender) {
+                io.emit("receive_message", messageData);
             }
+        } else {
+            io.emit("receive_message", messageData); 
+        }
+    });
+
+    socket.on("delete_message", (id) => {
+        db.query("DELETE FROM chats WHERE id = ?", [id], (err) => {
+            if (!err) io.emit("message_deleted", id);
+        });
+    });
+
+    socket.on("clear_all_chat", () => {
+        db.query("DELETE FROM chats", (err) => {
+            if (!err) io.emit("chat_cleared");
         });
     });
 
     socket.on("disconnect", () => {
         if (socket.username) {
             delete onlineUsers[socket.username];
-            io.emit("update_user_list", Object.keys(onlineUsers));
+            io.emit("update_lists", { users: Object.keys(onlineUsers), groups: Object.keys(protectedGroups) });
         }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
+server.listen(PORT, () => console.log(`Nebula Core Online: ${PORT}`));
